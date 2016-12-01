4a33745ce3875d6e507d960766d336425e4d4d31
nayadett1234 <nayadett.aguirre@gmail.com>
2016-12-01 19:33:49 -0300

    con validadores

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
+  validates :email, presence: { message: "Campo obligatorio" }
+  validates :email , uniqueness:{ message: " Mail ya existe , prueba con otro" , case_sensitive: false }
+
+
 def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
